4f3eb5486be26ebd854a77cf154668f8d2627db2
NASTEK\luiz.silva <luiz.silva@nastek.com.br>
2018-01-05 13:05:09 -0300

    Adicionado máscara para EditText.

--- app/src/main/java/br/com/studio/studiopilates/PacientesActivity.java
+++ app/src/main/java/br/com/studio/studiopilates/PacientesActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class PacientesActivity extends AppCompatActivity {
@@ -16,6 +17,7 @@ public class PacientesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pacientes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_paciente);
 
@@ -28,4 +30,17 @@ public class PacientesActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 }
--- app/src/main/java/br/com/studio/studiopilates/fragment/DadosPessoaisFragment.java
+++ app/src/main/java/br/com/studio/studiopilates/fragment/DadosPessoaisFragment.java
@@ -1,15 +1,24 @@
 package br.com.studio.studiopilates.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.redmadrobot.inputmask.MaskedTextChangedListener;
+
+import br.com.studio.studiopilates.MainActivity;
 import br.com.studio.studiopilates.R;
 
 
 public class DadosPessoaisFragment extends Fragment {
+
+    private View fragmentView;
+
+    private EditText telefone;
 
     public DadosPessoaisFragment() {
     }
@@ -17,8 +26,35 @@ public class DadosPessoaisFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.dados_pessoais_fragment, container, false);
+        fragmentView = inflater.inflate(R.layout.dados_pessoais_fragment, container, false);
 
-        return rootView;
+        getComponents();
+        setMaskTelefone();
+
+        return fragmentView;
+    }
+
+    private void getComponents(){
+        telefone = (EditText) fragmentView.findViewById(R.id.dados_pessoais_telefone);
+    }
+
+    private void setMaskTelefone(){
+        final MaskedTextChangedListener listener = new MaskedTextChangedListener(
+                "+7 ([000]) [000] [00] [00]",
+                true,
+                telefone,
+                null,
+                new MaskedTextChangedListener.ValueListener() {
+                    @Override
+                    public void onTextChanged(boolean maskFilled, @NonNull final String extractedValue) {
+//                        Log.d(MainActivity.class.getSimpleName(), extractedValue);
+//                        Log.d(MainActivity.class.getSimpleName(), String.valueOf(maskFilled));
+                    }
+                }
+        );
+
+        telefone.addTextChangedListener(listener);
+        telefone.setOnFocusChangeListener(listener);
+        telefone.setHint(listener.placeholder());
     }
 }
